f23c6e78c23b93c59816cb29fbce6f64cbe2297e
Sanskruti Kadam <sanskrutimk02@gmail.com>
2023-07-08 02:27:43 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import "../src/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Experience from "./pages/Experience";
-import Projects from "./pages/Project";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/projects" element={<Projects />}/>
-          <Route path="/experience" element={<Experience />}/>
-        </Routes>
-      </Router>
-    </div>
+    <></>
   );
 }
 
